b169822be18b0dbcf35895f0558c17dd7d928a84
Adity Roy <adity.roy@neotechelements.com>
2026-02-05 19:34:50 +0600

    Design Website Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,40 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="bg-black text-white px-8 py-4 flex gap-6">
-      <Link to="/" className="hover:text-blue-400">Home</Link>
-      <Link to="/about" className="hover:text-green-400">About</Link>
+    <nav className="fixed top-0 left-0 w-full bg-blue-500 text-white z-50">
+      <div className="max-w-7xl mx-auto px-10 py-4 flex items-center justify-between">
+
+        {/* Logo from public */}
+        <Link to="/">
+          <img
+            src="/logo.png"
+            className="h-10 w-auto"
+          />
+        </Link>
+
+        {/* Menu */}
+        <div className="flex gap-4 text-sm font-semibold">
+          <Link to="/" className="px-4 py-2  rounded hover:bg-blue-700">
+            HOME
+          </Link>
+          <Link to="/about" className="px-4 py-2 rounded hover:bg-blue-700">
+            ABOUT
+          </Link>
+          <Link to="/services" className="px-4 py-2 rounded hover:bg-blue-700">
+            CAREER
+          </Link>
+          <Link to="/contact" className="px-4 py-2 rounded hover:bg-blue-700">
+            CONTACT 
+          </Link>
+          <Link to="/feedback" className="px-4 py-2 rounded hover:bg-blue-700">
+            FEEDBACK
+          </Link>
+        </div>
+
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
-
+export default Navbar;
